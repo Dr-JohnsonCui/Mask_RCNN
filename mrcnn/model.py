c7b7e0ce8256fd7031cd22033772c58d75d82367
Dr-JohnsonCui <101709776+Dr-JohnsonCui@users.noreply.github.com>
2023-03-15 16:09:41 +0800

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -784,7 +784,7 @@ def refine_detections_graph(rois, probs, deltas, window, config):
     return detections
 
 
-class DetectionLayer(KE.Layer):
+class DetectionLayer(KE):
     """Takes classified proposal boxes and their bounding box deltas and
     returns the final detection boxes.
 
