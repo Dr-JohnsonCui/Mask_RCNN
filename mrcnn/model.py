1cccb66b6f03d28d79e391ff40c9898097d73c8d
Dr-JohnsonCui <101709776+Dr-JohnsonCui@users.noreply.github.com>
2023-03-15 14:41:02 +0800

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -20,8 +20,13 @@ import tensorflow as tf
 import keras
 import keras.backend as K
 import keras.layers as KL
-import keras.engine as KE
+#import keras.engine as KE
 import keras.models as KM
+
+import keras.engine.topology as KE
+
+
+
 
 from mrcnn import utils
 
